6e1b57d7bc6622f75329fe6caf664d4288b3e9ea
Kumaresan <rskumaresan2013@gmail.com>
2022-09-23 21:00:08 +0530

    Stage 2 commit

---
+++ NZWalks/NZWalks/Data/NZWalksDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.Models.Domain;
+using System.Data.Common;
+
+namespace NZWalks.Data
+{
+    public class NZWalksDBContext: DbContext    
+    {
+        public NZWalksDBContext(DbContextOptions<NZWalksDBContext> options) : base(options)
+
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+               public DbSet<WalkDifficulty> WalkDifficultys { get; set; }
+
+    }
+}
